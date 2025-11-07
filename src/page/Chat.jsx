2194f54c1e43613743e67935c06c63501f71f091
yoots50 <202210947@sangmyung.kr>
2025-11-07 23:11:48 +0900

    new annotations for changed logics

--- src/page/Chat.jsx
+++ src/page/Chat.jsx
@@ -1,39 +1,63 @@
-import React, { useEffect, useState } from "react";
-import SockJS from "sockjs-client"; // 
+import React, { useEffect, useRef, useState } from "react";
+import SockJS from "sockjs-client";
 import { over } from "stompjs";
 
-let stompClient = null; // 이곳에 over함수를 통해 SockJS함수의 socket을 client로 받아 넣음
-
 export default function Chat() {
-  const [chat, setChat] = useState([]); // 유저들의 채팅 내용을 가진 리스트, 리스트 안에는 {type: ..., message: ..., nickname: ...}와 같은 UserDTO의 형태를 띄는 객체가 들어간다.
-  const [value, setValue] = useState(""); // 유저가 보낼 메시지 내용
-  const [isConnected, setIsConnected] = useState(false); // 유저가 stompClient에 접속했는지 여부를 저장
-  const nickname = localStorage.getItem("nickname"); // 유저가 입력한 닉네임을 로컬 스토리지에서 뽑아옴
-  const handleChange = (e) => { // 유저가 input태그로 메시지를 입력할 시 value를 바꾸기 위함
-    setValue(e.target.value); // input 태그 속 값을 value에 넣음
+  const [chats, setChats] = useState([]); // 유저가 입력할 채팅 내용
+  const [value, setValue] = useState(""); // 입력창의 값
+  const [users, setUsers] = useState([]); // 접속한 유저 리스트
+  const stompClientRef = useRef(null); // stompClient를 담을 ref
+  const nickname = localStorage.getItem("nickname"); // 유저가 입력한 닉네임
+  const handleChange = (e) => {
+    setValue(e.target.value);
   };
   const handleSubmit = async (e) => {
-    e.preventDefault(); // submit을 하면 새로고침이 되는 것을 방지
+    e.preventDefault(); // 폼 제출 시 새로고침 방지
+    // !!!!! 코드 작성하는 곳 !!!!!
     // 메세지를 서버로 전송하는 로직을 작성할 것
+    // 전송할 때는 stompClientRef.current.send를 사용할 것
+    // 전송할 때의 형식은 ChatDTO 형태로 보낼 것 {type, message, nickname}
+    // type은 MESSAGE, message는 유저가 입력한 값, nickname은 유저의 닉네임
+    // 예시)
+    // const msgToSend = {type: "MESSAGE", message: value, nickname: nickname};
     // messageType는 MESSAGE로 할 것
-    // 메세지를 서버로 전송했다면 setValue
+    // send할 때는 JSON.stringify(msgToSend)를 사용할 것
+    // send할 destination은 "/app/send/messages"로 할 것
+    setValue(""); // 전송 후 입력창을 비우는 로직
   };
-  // useEffect를 통해 서버와 연결하는 로직 작성
-  // 서버와 연결할 시 messageType를 ENTER로 하는 ChatDTO 전송, message는 null로 할 것
-  // 서버와 연결이 끊어질 시 messageType를 LEAVE로 하는 chatDTO 전송, message는 null로 할 것
-  // SockJs와 stompjs의 over함수, isConnected, stompClient 등을 사용 할 것
+  useEffect(() => {
+    // !!!!! 코드 작성하는 곳 !!!!!
+    // useEffect안에 통해 서버와 연결하고 연결을 끊는 로직 작성
+    // 서버와 연결할 시 messageType를 ENTER로 하는 ChatDTO 전송
+    // 서버와 연결이 끊어질 시 messageType를 LEAVE로 하는 chatDTO 전송
+    // stompClientRef.current를 통해 stompClient에 접근 가능
+    // 서버와 연결할 땐 stompClientRef.current.connect({}, onConnected, onError);을 사용
+    // 연결이 끊어질 땐 stompClientRef.current.disconnect();를 사용
+    // onConnected 함수 안에서 "/topic/messages"와 "/topic/users"를 구독하고
+    // 구독한 후 "/topic/messages"로부터 메세지를 받으면 chat 상태를 업데이트
+    // "/topic/users"로부터 유저 리스트를 받으면 users 상태를 업데이트
+    // onConnected 함수가 실행되면 서버로 ENTER 메시지를 전송
+    // 구독은 stompClientRef.current.subscribe을 사용
+    // 메시지를 전송할 때는 JSON.stringify(msgToSend)를 사용
+    // 메시지를 받을 때는 JSON.parse(message.body)를 사용
+    // disconnect 함수는 useEffect가 return할 때 작성
+    // disconnect 함수를 쓰기 전 서버로 LEAVE 메시지를 전송
+    // ENTER, LEAVE 메시지를 send할 destination은 "/app/send/users"로 할 것
+  }, []);
+
   return (
     <div>
-      <form onSubmit={handleSubmit}> {/* send 버튼을 누를 시 handleSubmit이 작동 */}
+      <form onSubmit={handleSubmit}>
         <input
           placeholder="type message here"
           type="text"
           value={value}
           onChange={handleChange}
-        /> {/* 유저가 메시지를 입력 할 수 있는 input태그 */}
-        <button>send</button> {/* 유저가 send 버튼을 누를 시 handleSubmit이 실행된다. */}
+        />
+        <button>send</button>
       </form>
-      {/* 이곳에 chat 안에 있는 유저들의 채팅 내용을 html 태그로 변환하여 보여준다. chat 안에는 UserDTO 형식의 객체가 있으므로 이를 활용한다.*/}
+      {users && users.map((user) => <h1>{user}</h1>)} {/* 접속한 유저 리스트 */}
+      {chats && chats.map((chat) => <h1>{chat}</h1>)} {/* 채팅 내용 */}
     </div>
   );
 }
