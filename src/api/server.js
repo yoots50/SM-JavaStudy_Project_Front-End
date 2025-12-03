c1040f5ec734c80c75b38a5b2215255fca95242b
yoots50 <202210947@sangmyung.kr>
2025-12-03 23:28:32 +0900

    SM auth added

--- src/api/server.js
+++ src/api/server.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export async function nicknameCheck(nickname) {
-  const URL = `http://${process.env.REACT_APP_SERVER_IP}:8080/user/nickname`;
+export async function auth(value) {
+  const URL = `http://${process.env.REACT_APP_SERVER_IP}:8080/user/auth`;
   return await axios
-    .post(URL, null, {params: {nickname}})
+    .post(URL, null, {params: value})
     .then((r) => r.data)
     .catch((e) => e);
 }
